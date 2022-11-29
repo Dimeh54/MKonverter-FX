c5dafd9bd2e04760b13f53b9c87128ad2ec5733f
Yghore <yhgore@gmail.com>
2022-11-29 20:25:06 +0100

    Test with libraries APACHE POI

--- src/main/java/fr/yghore/Main.java
+++ src/main/java/fr/yghore/Main.java
@@ -1,7 +1,30 @@
 package fr.yghore;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.List;
+
+import org.apache.poi.openxml4j.opc.PackagePart;
+import org.apache.poi.xwpf.usermodel.BodyElementType;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void readDocxFile(String fileName) {
+        try {
+            File file = new File(fileName);
+            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+            XWPFDocument document = new XWPFDocument(fis);
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+            for (XWPFParagraph paragraph : paragraphs) {
+                System.out.println(paragraph.getParagraphText())
+            }
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+    public static void main(String[] args) {
+        readDocxFile("TEST.docx");
+    }
+} 
